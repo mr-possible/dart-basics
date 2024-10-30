f32ac5c7de4a45c59fccea0a62fde318609d34c6
Sambhav Dave <sambhav@xogene.com>
2024-10-30 10:38:22 +0530

    data types and variables

---
+++ first.dart
@@ -0,0 +1,21 @@
+// Data types and variables.
+
+void main() {
+  
+  String name = "Sambhav";
+  
+  bool male = true;
+
+  int age = 21;
+
+  List<String> fruits = ["mango", "apple"];
+
+  Map<String, dynamic> data = {
+    "name": name,
+    "male": male,
+    "age": age,
+    "fruits": fruits
+  };
+
+  print(data);
+}
